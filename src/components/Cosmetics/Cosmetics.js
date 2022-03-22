7e6015cca29c8e7966e5a1089367898a5da2f0e6
tanverrana <tanversobur@gmail.com>
2022-03-23 00:56:21 +0600

    cosmetics

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//import add from '../../utilities/calculate';
+
+const Cosmetics = () => {
+    const cosmetics = [
+        { id: 1, name: "Alta", price: 100 },
+        { id: 2, name: "Payel", price: 200 },
+        { id: 3, name: "malish", price: 300 },
+        { id: 4, name: "Balish", price: 400 },
+        { id: 5, name: "nalish", price: 500 },
+    ]
+    return (
+        <div>
+            <h1>Welcome to my Cosmetics.</h1>
+            {
+                cosmetics.map(cosmetic => console.log(cosmetic))
+            }
+
+        </div>
+    );
+};
+
+export default Cosmetics;
